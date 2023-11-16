6b706fe95cff5ef392b5f46f2657c37ccea92f7e
PutraGandaD <putraganda.dewata@gmail.com>
2023-11-16 15:30:03 +0700

    Pertemuan 16 Nov 2023

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layout.main')
+@section('title', 'Mahasiswa')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Mahasiswa</h4>
+                    <p class="card-description">
+                        Daftar mahasiswa Universitas MDP
+                    </p>
+                    <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary btn-rounded btn-fw">Tambah</a>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th>NPM</th>
+                                    <th>Nama</th>
+                                    <th>Tempat Lahir</th>
+                                    <th>Tanggal Lahir</th>
+                                    <th>Foto</th>
+                                    <th>Nama Prodi</th>
+                                    <th>Nama Fakultas</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($mahasiswa as $item)
+                                    <tr>
+                                        <td>{{ $item['npm'] }}</td>
+                                        <td>{{ $item['nama'] }}</td>
+                                        <td>{{ $item['tempat_lahir'] }}</td>
+                                        <td>{{ $item['tanggal_lahir'] }}</td>
+                                        <td>
+                                            <img src="foto/{{ $item['foto'] }}" class="rounded-circle" width="70px" />
+                                        </td>
+                                        <td>{{ $item['prodi']['nama'] }}</td>
+                                        <td>{{ $item['prodi']['fakultas']['nama'] }}</td>
+                                        <td>
+                                            <div class="d-flex justify-content-center">
+                                                <a href="{{ route('mahasiswa.edit', $item->id) }}">
+                                                    <button class="btn btn-success btn-sm mx-3">Edit</button>
+                                                </a>
+                                                <form method="post" action="{{ route('mahasiswa.destroy', $item->id) }}">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        @if (Session::get('success'))
+            toastr.success("{{ Session::get('success') }}");
+        @endif
+    </script>
+@endsection
---
+++ resources/views/prodi/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.main')
+@section('title', 'Tambah Program Studi')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Tambah Program Studi</h4>
+                    <p class="card-description">
+                        Formulir tambah program studi
+                    </p>
+                    <form class="forms-sample" method="POST" action="{{ route('prodi.store') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="nama">Nama Program Studi</label>
+                            <input type="text" class="form-control" name="nama" placeholder="Nama Program Studi">
+                            @error('nama')
+                                <label class="text-danger">{{ $message }}</label>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="fakultas_id">Nama Fakultas</label>
+                            <select name="fakultas_id" class="form-control">
+                                <option value="">Pilih</option>
+                                @foreach ($fakultas as $item)
+                                    <option value="{{ $item->id }}"> {{ $item->nama }} </option>
+                                @endforeach
+                            </select>
+                            @error('fakultas_id')
+                                <label class="text-danger">{{ $message }}</label>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary mr-2">Simpan</button>
+                        <a href="{{ url('prodi') }}" class="btn btn-light">Batal</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
